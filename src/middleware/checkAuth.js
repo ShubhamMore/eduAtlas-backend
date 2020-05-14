7668e81a35d68d07122938c01119d0ad9be37da2
Akash Kulkarni <akashkulkarni796@gmail.com>
2020-05-14 10:14:50 +0530

    akash

--- src/middleware/checkAuth.js
+++ src/middleware/checkAuth.js
@@ -17,15 +17,8 @@ const auth = async (req, res, next) => {
     req.user = user;
     console.log(token);
     next();
-<<<<<<< HEAD
-  } catch (error) {
-    console.log(error);
-    res.status(401).json({ message: 'Token is not valid or not provided' });
-    return;
-=======
   } catch (e) {
     res.status(401).send({ error: 'Please authenticate.' });
->>>>>>> 96fa889761c5abefe1fbb626c3f0add6dcfb7e8e
   }
 };
 
