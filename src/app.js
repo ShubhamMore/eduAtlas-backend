3936742ab59da314d84625180e9f0c5ed2a51e50
Akash Kulkarni <akashkulkarni796@gmail.com>
2020-05-17 13:11:14 +0530

    akash

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname + '/dist')));
+app.use(express.static(path.join(__dirname,'../dist')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
