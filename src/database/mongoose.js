07361ec51f4dd4faf39f17239d759d3aa14c8cc0
Akash Kulkarni <akashkulkarni796@gmail.com>
2020-05-19 12:49:42 +0530

    akash

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const MONGO_SERVER = "mongodb+srv://userDB:aaouikika@bingbot-3awwl.mongodb.net/Eduatlas?retryWrites=true"
-mongoose.connect(MONGO_SERVER, {
+const MONGO_SERVER_LOCAL = "mongodb://127.0.0.1:27017/Eduatlas"
+mongoose.connect(MONGO_SERVER_LOCAL, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
