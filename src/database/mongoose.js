e84c4f80602728269ca38482632d2caf908ed1c5
Shubham More <shubhammore1796@gmail.com>
2020-05-19 14:30:29 +0530

    eduatlas

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const MONGO_SERVER = "mongodb+srv://userDB:aaouikika@bingbot-3awwl.mongodb.net/Eduatlas?retryWrites=true"
+// const MONGO_SERVER = "mongodb+srv://userDB:aaouikika@bingbot-3awwl.mongodb.net/Eduatlas?retryWrites=true"
+const MONGO_SERVER = process.env.MONGODB_LOCAL_URL;
 mongoose.connect(MONGO_SERVER, {
   useNewUrlParser: true,
   useCreateIndex: true,
