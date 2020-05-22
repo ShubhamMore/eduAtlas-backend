c4593ea281928fb921c740b726fb3f2ba1aecefa
Shubham More <shubhammore1796@gmail.com>
2020-05-22 12:33:21 +0530

    eduatlas

--- src/routes/student.js
+++ src/routes/student.js
@@ -3,24 +3,24 @@ const checkAuth = require('../middleware/checkAuth');
 const studentRouter = require('express').Router({ caseSensitive: true });
 const studentController = require('../controller/student');
 //to add new student - 1. create new user 2. add student
-studentRouter.post('/add', studentController.addStudent);
+studentRouter.post('/add', studentController.addStudent); //done
 
 //Add course to existing student
-studentRouter.post('/addCourseStudent', studentController.addCourseStudent);
+studentRouter.post('/addCourseStudent', studentController.addCourseStudent); //done
 
 //to get list of active student of a courese
-studentRouter.post('/getActiveStudents', studentController.getActiveStudents);
+studentRouter.post('/getActiveStudents', studentController.getActiveStudents); //not done
 
 //to get pending students
-studentRouter.post('/getPendingStudents', studentController.getPendingStudents);
+studentRouter.post('/getPendingStudents', studentController.getPendingStudents); //done
 
 //to remove course from the student
 studentRouter.post('/deleteStudentCourse', studentController.deleteStudentCourse);
 
 //to get have course details of student
-studentRouter.post('/getOneStudentByInstitute', studentController.getOneStudentByInstitute);
+studentRouter.post('/getOneStudentByInstitute', studentController.getOneStudentByInstitute); //done
 
-studentRouter.post('/getOneStudent', studentController.getOneStudent);
+studentRouter.post('/getOneStudent', studentController.getOneStudent); //done
 
 studentRouter.delete('', checkAuth, studentController.deleteStudent);
 
