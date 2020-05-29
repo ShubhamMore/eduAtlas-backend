6c10286ef1dfadc99df1940240e22a527454011f
Shubham More <shubhammore1796@gmail.com>
2020-05-29 10:19:04 +0530

    eduatlas

--- src/controller/fee.js
+++ src/controller/fee.js
@@ -1,106 +1,108 @@
 const errorHandler = require('../service/errorHandler');
 const User = require('../model/user.model');
 const EduAtlasId = require('../model/eduatlasId.model');
-const Institute = require('../model/institute.model')
+const Institute = require('../model/institute.model');
 const Student = require('../model/schedule.model');
-const Fee = require('../model/fee.model')
+const Fee = require('../model/fee.model');
 
+exports.addFee = async (req, res) => {
+  try {
+    const checkStudent = await Student.findOne({
+      $and: [
+        {
+          studentId: req.body.studentId,
+        },
+        {
+          'instituteDetails._id': req.body.instituteId,
+        },
+      ],
+    });
 
-exports.addFee = async (req,res)=>{
-    try {
-        const checkStudent = await Student.findOne({
-            $and:[{
-                studentId:req.body.studentId
-            },{
-                'instituteDetails._id':req.body.instituteDetailsId
-            }]
-        })
-
-        if(!checkStudent){
-            const error = new Error('Course for Student doesnt exists');
-            //error.prototype.statusCode = 400;
-            throw error;
-        }
-
-        const fee = new Fee(req.body)
-        await fee.save()
-    } catch (error) {
-        response(res, 500, error.message);
-
+    if (!checkStudent) {
+      const error = new Error('Course for Student doesnt exists');
+      //error.prototype.statusCode = 400;
+      throw error;
     }
-}
 
-exports.getFeeOfStudent= async(req,res)=>{
-    try {
-        const studentFee = await Fee.find({
-            studentId:req.body.studentId
-        })
+    const fee = new Fee(req.body);
+    await fee.save();
+  } catch (error) {
+    response(res, 500, error.message);
+  }
+};
 
-        if(studentFee.length == 0){
-            const error = new Error('No Student Fee Available');
-            error.statusCode = 400;
-            throw error;
-        }
+exports.getFeeOfStudent = async (req, res) => {
+  try {
+    const studentFee = await Fee.find({
+      studentId: req.body.studentId,
+    });
 
-        res.status(200).send(studentFee)
-    } catch (error) {
-        
+    if (studentFee.length == 0) {
+      const error = new Error('No Student Fee Available');
+      error.statusCode = 400;
+      throw error;
     }
-}
-exports.getFeeOfStudentByCourse = async(req,res)=>{
-    try {
-        const studentCourseFee = await Fee.find({
-            studentId:req.body.studentId,
-            instituteDetailsId:req.body.instituteDetailsId
-        })
 
-        res.status(200).send(studentCourseFee)
-    } catch (error) {
-        
+    res.status(200).send(studentFee);
+  } catch (error) {}
+};
+exports.getFeeOfStudentByCourse = async (req, res) => {
+  try {
+    const studentCourseFee = await Fee.find({
+      studentId: req.body.studentId,
+      instituteDetailsId: req.body.instituteDetailsId,
+    });
+
+    res.status(200).send(studentCourseFee);
+  } catch (error) {}
+};
+
+exports.getFeeByCourse = async (req, res) => {
+  try {
+    const CourseFeeDetails = await Fee.find({
+      instituteDetailsId: req.body.instituteDetailsId,
+    });
+    res.status(200).send();
+  } catch (error) {}
+};
+
+exports.updateFeeOfStudent = async (req, res) => {
+  try {
+    const check = await Fee.findOne({
+      $and: [
+        {
+          studentId: req.body.studentId,
+        },
+        {
+          instituteDetailsId: req.body.instituteDetailsId,
+        },
+      ],
+    });
+
+    if (!check) {
+      const error = new Error('No Student Fee Available');
+      error.statusCode = 400;
+      throw error;
     }
-}
 
-exports.getFeeByCourse = async(req,res)=>{
-    try {
-        const CourseFeeDetails = await Fee.find({
-            instituteDetailsId:req.body.instituteDetailsId
-        })
-        res.status(200).send()
-    } catch (error) {
-        
-    }
-}
+    const updateFee = await Fee.updateOne(
+      {
+        $and: [
+          {
+            _id: req.body.feeId,
+          },
+          {
+            'installments.installmentId': req.body.installment._id,
+          },
+        ],
+      },
+      {
+        $set: {
+          installments: req.body.installments,
+        },
+      }
+    );
 
-exports.updateFeeOfStudent = async(req,res)=>{
-    try {
-        const check = await Fee.findOne({
-            $and:[{
-                studentId:req.body.studentId
-            },{
-                instituteDetailsId:req.body.instituteDetailsId
-            }]
-        })
-        
-        if(!check){
-            const error = new Error('No Student Fee Available');
-            error.statusCode = 400;
-            throw error;
-        }
-
-        const updateFee = await Fee.updateOne({
-            $and:[{
-                _id:req.body.feeId
-            },{
-                'installments.installmentId':req.body.installment._id
-            }]
-        },{
-            $set:{
-                installments:req.body.installments
-            }
-        })
-
-        res.status(200).send(updateFee)
-    } catch (error) {
-        
-    }
-}
+    res.status(200).send(updateFee);
+  } catch (error) {}
+};
--- src/model/fee.model.js
+++ src/model/fee.model.js
@@ -2,57 +2,59 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const feeSchema = new Schema({
-        instituteDetailsId:{
-            type:String
-        },
-        instituteId:{
-            type:String
-        },
-        studentId:{
-            type:String
-        },
-        courseId:{
-            type:String
-        },
-        date:{
-            type:String
-        },
-        totalAmount:{
-            type:String
-        },
-        amountCollected:{
-            type:String
-        },
-        installmentType:{
-            type:String
-        } ,
-        noOfInstallments:{
-            type:String
-        },
-        installments: [
-            {
-                paidStatus:{
-                    type:Boolean,
-                    default:false
-                },
-                installmentNo:{
-                    type:String
-                },
-                paidOn:{
-                    type:String
-                },
-                amount:{
-                    type:String
-                },
-                paymentMode:{
-                    type:String
-                },
-                amountPending:{
-                    type:String
-                }
-                
-            }
-        ]     
-})
+  instituteDetailsId: {
+    type: String,
+  },
+  instituteId: {
+    type: String,
+  },
+  studentId: {
+    type: String,
+  },
+  courseId: {
+    type: String,
+  },
+  date: {
+    type: String,
+  },
+  totalAmount: {
+    type: String,
+  },
+  amountCollected: {
+    type: String,
+  },
+  installmentType: {
+    type: String,
+  },
+  pendingAmount: {
+    type: String,
+  },
+  noOfInstallments: {
+    type: String,
+  },
+  installments: [
+    {
+      paidStatus: {
+        type: String,
+        default: 'false',
+      },
+      installmentNo: {
+        type: String,
+      },
+      paidOn: {
+        type: String,
+      },
+      amount: {
+        type: String,
+      },
+      paymentMode: {
+        type: String,
+      },
+      amountPending: {
+        type: String,
+      },
+    },
+  ],
+});
 
-module.exports = mongoose.model('Fee', feeSchema)
+module.exports = mongoose.model('Fee', feeSchema);
