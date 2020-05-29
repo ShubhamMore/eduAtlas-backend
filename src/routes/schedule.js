5774cfe65e8f26d8415674f458cb0da74bb13b6d
Shubham More <shubhammore1796@gmail.com>
2020-05-29 14:16:08 +0530

    eduatlas

--- src/routes/schedule.js
+++ src/routes/schedule.js
@@ -1,14 +1,14 @@
 const checkAuth = require('../middleware/checkAuth');
-const scheduleRouter = require('express').Router({ caseSensitive: true });
-const scheduleController = require('../controller/schedule')
+const scheduleRouter = require('express').Router();
+const scheduleController = require('../controller/schedule');
 
 //@schedule_Routes
 scheduleRouter.post('/addSchedule', scheduleController.addSchedule);
 
-scheduleRouter.post('/updateSchedule',  scheduleController.updateSchedule);
+scheduleRouter.post('/updateSchedule', scheduleController.updateSchedule);
 
-scheduleRouter.post('/getScheduleByBatch',scheduleController.getScheduleByBatch)
+scheduleRouter.post('/getScheduleByBatch', scheduleController.getScheduleByBatch);
 
-scheduleRouter.post('/getSchedule',  scheduleController.getSchedule);
+scheduleRouter.post('/getSchedule', scheduleController.getSchedule);
 
-module.exports = schdeuleRouter
+module.exports = scheduleRouter;
