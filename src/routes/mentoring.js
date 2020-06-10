a95fba3dbc53b98ab1abfaca430ffee512680d8f
Akash Kulkarni <akashkulkarni796@gmail.com>
2020-06-10 22:39:16 +0530

    akash

--- src/routes/mentoring.js
+++ src/routes/mentoring.js
@@ -4,6 +4,9 @@ const checkAuth = require('../middleware/checkAuth')
 
 mentorRouter.post('/addMentoring',checkAuth, mentorController.addMentoring)
 mentorRouter.post('/getMentoringByInstitute',checkAuth, mentorController.getMentoringByInstitute)
+mentorRouter.post('/getMentoringOfStudentByInstitute',checkAuth, mentorController.getMentoringOfStudentByInstitute)
+mentorRouter.post('/updateMentoring',checkAuth,mentorController.updateMentoring)
+mentorRouter.post('/deleteMentoring',checkAuth,mentorController.deleteMentoring)
 
 
 module.exports = mentorRouter
