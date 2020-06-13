9aea41783c6c463bc538c0a8897b3994e2eb392f
Shubham More <shubhammore1796@gmail.com>
2020-06-13 18:14:05 +0530

    eduatlas

--- src/controller/forum.js
+++ src/controller/forum.js
@@ -3,7 +3,7 @@ const Institute = require('../model/institute.model');
 
 const response = require('../service/response');
 const errorHandler = require('../service/errorHandler');
-<<<<<<< HEAD
+const mongoose = require('mongoose');
 
 exports.addForum = async (req, res) => {
   try {
@@ -16,179 +16,6 @@ exports.addForum = async (req, res) => {
       error.statusCode = 400;
       throw error;
     }
-=======
-const mongoose = require('mongoose');
-
-exports.addForum = async(req,res)=>{
-    try {
-        const instituteCheck = await Institute.findOne({
-            _id:req.body.instituteId
-        })
-
-        if(!instituteCheck){
-            const error = new Error("Institute Not available")
-            error.statusCode = 400
-            throw error
-        }
-
-        const newForum = new Forum(req.body)
-        await newForum.save()
-        res.status(200).send(newForum)
-    } catch (error) {
-        errorHandler(error,res)
-    }
-}
-
-exports.getForumsByInstitute = async(req,res)=>{
-    try {
-        let query = {}
-        
-        if(!req.body.courseId){
-            query = {
-                instituteId:req.body.instituteId
-            }
-        } else {
-            query = {    
-                instituteId:req.body.instituteId,
-                courseId:req.body.courseId,
-            }
-        }         
-             
-        const myForums = await Forum.find(query)
-        for(var i =0;i< myForums.length ;i++){
-            let obj = {}
-            if(myForums[i].courseId){
-                const courseName = await Institute.aggregate([{
-                    $match:{
-                        _id: mongoose.Types.ObjectId(myForums[i].instituteId)
-                    }
-                  },{
-                        $unwind:"$course"
-                    },{
-                        $match:{
-                            "course._id":mongoose.Types.ObjectId(myForums[i].courseId)
-                        }
-                    }
-                ])
-                myForums[i].courseId = courseName[0].course.name
-                
-            }
-        }    
-        res.status(200).send(myForums)      
-    } catch (error) {
-        res.status(400).send(error)       
-    }
-}
-exports.getMyForum = async(req,res)=>{
-    try {
-
-        let query = {}
-        
-        if(!req.body.courseId){
-            query = {
-                createdBy:req.body.createdBy
-            }
-        } else {
-            query = {    
-                createdBy:req.body.createdBy,
-                courseId:req.body.courseId,
-            }
-        }
-
-        const myForums = await Forum.find(query)
-        let forum = [{}]
-        for(var i =0;i< myForums.length ;i++){
-            let obj = {}
-            if(myForums[i].courseId){
-                const courseName = await Institute.aggregate([{
-                    $match:{
-                        _id: mongoose.Types.ObjectId(myForums[i].instituteId)
-                    }
-                  },{
-                        $unwind:"$course"
-                    },{
-                        $match:{
-                            "course._id":mongoose.Types.ObjectId(myForums[i].courseId)
-                        }
-                    }
-                ])
-                myForums[i].courseId = courseName[0].course.name
-                
-            }
-        }
-
-        res.status(200).send(myForums)
-    } catch (error) {
-        errorHandler(error,res)
-    }
-}
-
-exports.getSingleForum = async(req,res)=>{
-    try {
-        
-        const singleForum = await Forum.findOne({
-            _id:req.body._id
-        })
-        let newforum = JSON.parse(JSON.stringify(singleForum))
-        if(singleForum.courseId){
-            const institute = await Institute.aggregate([
-                {
-                    $unwind:"$course"
-                },{
-                    $match:{
-                        _id:mongoose.Types.ObjectId(singleForum.instituteId),
-                        "course._id":mongoose.Types.ObjectId(singleForum.courseId)
-                    }
-                }
-            ])
-
-            newforum.courseName = institute[0].course.name
-        }
-         
-        res.status(200).send(newforum)
-    } catch (error) {
-        errorHandler(error,res)
-    }
-}
-
-exports.addComment = async(req,res)=>{
-    try {
-        const comment = await Forum.updateOne({
-            _id:req.body._id
-        },{
-            $push:{
-                comments: req.body.comment
-            }
-        })
-        if(comment.ok == 0){
-            const error = new Error("Comment was not added")
-            error.statusCode = 202
-            throw error
-        }
-
-        res.status(200).send(comment)
-
-    } catch (error) {
-        errorHandler(error,res)
-    }
-}
-
-exports.updateForum = async(req,res)=>{
-    try {
-        const updateForum = await Forum.updateOne({
-            _id:req.body._id
-        },req.body)
-
-        if(updateForum.ok == 0){
-            const error = new Error("Forum was not added")
-            error.statusCode = 202
-            throw error
-        }
-        res.status(200).send(updateForum)
-
-    } catch (error) {
-        errorHandler(error,res)
->>>>>>> 04f951590a37154d859ace700e70fe3ca3fdd102
 
     const newForum = new Forum(req.body);
     await newForum.save();
@@ -212,17 +39,72 @@ exports.getForumsByInstitute = async (req, res) => {
         courseId: req.body.courseId,
       };
     }
-    const listForum = await Forum.find(query);
-    res.status(200).send(listForum);
+
+    const myForums = await Forum.find(query);
+    for (var i = 0; i < myForums.length; i++) {
+      let obj = {};
+      if (myForums[i].courseId) {
+        const courseName = await Institute.aggregate([
+          {
+            $match: {
+              _id: mongoose.Types.ObjectId(myForums[i].instituteId),
+            },
+          },
+          {
+            $unwind: '$course',
+          },
+          {
+            $match: {
+              'course._id': mongoose.Types.ObjectId(myForums[i].courseId),
+            },
+          },
+        ]);
+        myForums[i].courseId = courseName[0].course.name;
+      }
+    }
+    res.status(200).send(myForums);
   } catch (error) {
     res.status(400).send(error);
   }
 };
 exports.getMyForum = async (req, res) => {
   try {
-    const myForums = await Forum.find({
-      createdBy: req.body.createdBy,
-    });
+    let query = {};
+
+    if (!req.body.courseId) {
+      query = {
+        createdBy: req.body.createdBy,
+      };
+    } else {
+      query = {
+        createdBy: req.body.createdBy,
+        courseId: req.body.courseId,
+      };
+    }
+
+    const myForums = await Forum.find(query);
+    let forum = [{}];
+    for (var i = 0; i < myForums.length; i++) {
+      let obj = {};
+      if (myForums[i].courseId) {
+        const courseName = await Institute.aggregate([
+          {
+            $match: {
+              _id: mongoose.Types.ObjectId(myForums[i].instituteId),
+            },
+          },
+          {
+            $unwind: '$course',
+          },
+          {
+            $match: {
+              'course._id': mongoose.Types.ObjectId(myForums[i].courseId),
+            },
+          },
+        ]);
+        myForums[i].courseId = courseName[0].course.name;
+      }
+    }
 
     res.status(200).send(myForums);
   } catch (error) {
@@ -235,8 +117,24 @@ exports.getSingleForum = async (req, res) => {
     const singleForum = await Forum.findOne({
       _id: req.body._id,
     });
+    let newforum = JSON.parse(JSON.stringify(singleForum));
+    if (singleForum.courseId) {
+      const institute = await Institute.aggregate([
+        {
+          $unwind: '$course',
+        },
+        {
+          $match: {
+            _id: mongoose.Types.ObjectId(singleForum.instituteId),
+            'course._id': mongoose.Types.ObjectId(singleForum.courseId),
+          },
+        },
+      ]);
 
-    res.status(200).send(singleForum);
+      newforum.courseName = institute[0].course.name;
+    }
+
+    res.status(200).send(newforum);
   } catch (error) {
     errorHandler(error, res);
   }
@@ -280,8 +178,6 @@ exports.updateForum = async (req, res) => {
       error.statusCode = 202;
       throw error;
     }
-<<<<<<< HEAD
-
     res.status(200).send(updateForum);
   } catch (error) {
     errorHandler(error, res);
@@ -302,53 +198,25 @@ exports.deleteForum = async (req, res) => {
 exports.deleteComment = async (req, res) => {
   try {
     let query = {};
-    if (!req.body.comment_id) {
-      query = {
-        _id: req.body._id,
-      };
-    } else {
-      query = [
-        {
-          id: req.body._id,
-        },
-        {
-          $pull: {
-            comments: {
-              _id: req.body.commentId,
-            },
-          },
-        },
-      ];
-=======
-    
-}
-
-exports.deleteComment = async(req,res)=>{
-    try {
-        let query={}
-        if(req.body.userId != req.user._id){
-            const error = new Error("You cant Delete this comment")
-            error.statusCode = 400
-            throw error
-        }
-
-        const deleteQuery = await Forum.updateOne({
-            _id:req.body._id
-        },{
-            $pull:{
-                comments:{
-                    _id:req.body.commentId
-                }
-                
-            }
-        })
-        res.status(200).send(deleteQuery)
-    } catch (error) {
-        errorHandler(error,res)   
->>>>>>> 04f951590a37154d859ace700e70fe3ca3fdd102
+    if (req.body.userId != req.user._id) {
+      const error = new Error('You cant Delete this comment');
+      error.statusCode = 400;
+      throw error;
     }
 
-    const deleteQuery = await Forum.updateOne(query);
+    const deleteQuery = await Forum.updateOne(
+      {
+        _id: req.body._id,
+      },
+      {
+        $pull: {
+          comments: {
+            _id: req.body.commentId,
+          },
+        },
+      }
+    );
+    res.status(200).send(deleteQuery);
   } catch (error) {
     errorHandler(error, res);
   }
