c4f3c0b6e9488017d2b02c9ed31f373090c992c0
Akash Kulkarni <akashkulkarni796@gmail.com>
2020-06-14 16:24:43 +0530

    akash

--- src/routes/chats.js
+++ src/routes/chats.js
@@ -1,6 +1,6 @@
 const chatRouter = require('express').Router();
 const chatController = require('../controller/chats');
 const checkAuth = require('../middleware/checkAuth');
-chatRouter.post('/getMembers', chatController.getMembers);
+chatRouter.post('/getMembers', checkAuth, chatController.getMembers);
 
 module.exports = chatRouter;
