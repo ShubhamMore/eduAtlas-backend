425bc2522d8a39b8b44b7158306c2eb9035cc5cc
Shubham More <shubhammore1796@gmail.com>
2020-06-14 18:59:19 +0530

    eduatlas

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -1,8 +1,8 @@
 const chatRouter = require('express').Router({ caseSensitive: true });
-
 const chatController = require('../controller/chat');
+const checkAuth = require('../middleware/checkAuth');
 
 //to Add new Employee
-chatRouter.post('/getChats', chatController.getChats);
+chatRouter.post('/getChats', checkAuth, chatController.getChats);
 
 module.exports = chatRouter;
