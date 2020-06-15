88b24d9b03dd49c73e3903cfe43c752f851b9cbc
Shubham More <shubhammore1796@gmail.com>
2020-06-15 22:38:18 +0530

    eduatlas

--- src/controller/leads.js
+++ src/controller/leads.js
@@ -1,83 +1,112 @@
-const Leads = require('../model/leads.model')
-const Institute = require('../model/institute.model')
+const Leads = require('../model/leads.model');
+const Institute = require('../model/institute.model');
 const response = require('../service/response');
 const errorHandler = require('../service/errorHandler');
 
-exports.addLead = async (req,res)=>{
-    try {
-        const checkInsstitute = await Institute.find({})
-        const newLead = new Leads(req.body)
-        await newLead.save()
-        res.status(200).send(newLead)
-    } catch (error) {
-        res.status(400).send(error)   
+exports.addLead = async (req, res) => {
+  try {
+    const checkInsstitute = await Institute.find({});
+    const newLead = new Leads(req.body);
+    await newLead.save();
+    res.status(200).send(newLead);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+exports.getLeadsByOfInstitute = async (req, res) => {
+  try {
+    let query = {};
+
+    if (!req.body.status && !req.body.courseId) {
+      query = {
+        instituteId: req.body.instituteId,
+      };
+    } else if (!req.body.status) {
+      query = {
+        instituteId: req.body.instituteId,
+        courseId: req.body.courseId,
+      };
+    } else if (!req.body.courseId) {
+      query = {
+        instituteId: req.body.instituteId,
+        status: req.body.status,
+      };
+    } else {
+      query = {
+        instituteId: req.body.instituteId,
+        courseId: req.body.courseId,
+        status: req.body.status,
+      };
     }
-}
 
-exports.getLeadsByOfInstitute = async(req,res)=>{
-    try {
-        let query= {}
+    const leads = await Leads.find(query);
+    res.status(200).send(leads);
+  } catch (error) {
+    errorHandler(error, res);
+  }
+};
 
-        if(!req.body.status && !req.body.courseId) {
-            query = {
-                instituteId:req.body.instituteId
-            }
-        } else if(!req.body.status) {
-            
-            query = {
-                instituteId:req.body.instituteId,
-                courseId:req.body.courseId
-            }
-        } else if(!req.body.courseId){
-            query = {
-                instituteId:req.body.instituteId,
-                status:req.body.status
-            }
-        } else {
-            query = {
-                instituteId:req.body.instituteId,
-                courseId:req.body.courseId,
-                status:req.body.status
-            } 
-        }
+exports.getSingleLead = async (req, res) => {
+  try {
+    const lead = await Leads.findOne({
+      _id: req.body._id,
+    });
 
-        const leads = await Leads.find(query)
-        res.status(200).send(leads)
-    } catch (error) {
-        errorHandler(error,res)   
+    if (!lead) {
+      throw new Error('Lead Not Found');
     }
-}
 
-exports.getSingleLead = async(req,res)=>{
-    try {
-        const lead = await Leads.findOne({
-            _id:req.body._id
-        })
-        res.status(200).send(lead)
-    } catch (error) {
-        errorHandler(error,res)
+    res.status(200).send(lead);
+  } catch (error) {
+    errorHandler(error, res);
+  }
+};
+
+exports.getSingleLeadToView = async (req, res) => {
+  try {
+    const lead = await Leads.findOne({
+      _id: req.body._id,
+    });
+
+    console.log(lead);
+
+    if (!lead) {
+      throw new Error('Lead Not Found');
     }
-}
 
-exports.updateLead = async(req,res)=>{
-    try {
-        const updateLead = await Leads.updateOne({
-            _id:req.body._id
-        },req.body)
-        res.status(200).send(updateLead)
-    } catch (error) {
-        
-    }
-}
+    let course = await Institute.findOne({ _id: lead.instituteId }, { course: 1, _id: 0 });
+    course = course.course;
+    course = course.filter((c) => c._id == lead.courseId);
 
-exports.deleteLead = async (req,res)=>{
-    try {
-        const deleteLead = await Leads.deleteOne({
-            _id:req.body._id
-        })
+    lead.courseId = course.courseCode;
 
-        res.status(200).send(deleteLead)
-    } catch (error) {
-        errorHandler(error,res)
-    }
-}
+    res.status(200).send(lead);
+  } catch (error) {
+    errorHandler(error, res);
+  }
+};
+
+exports.updateLead = async (req, res) => {
+  try {
+    const updateLead = await Leads.updateOne(
+      {
+        _id: req.body._id,
+      },
+      req.body
+    );
+    res.status(200).send(updateLead);
+  } catch (error) {}
+};
+
+exports.deleteLead = async (req, res) => {
+  try {
+    const deleteLead = await Leads.deleteOne({
+      _id: req.body._id,
+    });
+
+    res.status(200).send(deleteLead);
+  } catch (error) {
+    errorHandler(error, res);
+  }
+};
--- src/routes/leads.js
+++ src/routes/leads.js
@@ -1,11 +1,12 @@
-const leadRouter = require('express').Router()
-const checkAuth = require('../middleware/checkAuth')
-const leadController = require('../controller/leads')
+const leadRouter = require('express').Router();
+const checkAuth = require('../middleware/checkAuth');
+const leadController = require('../controller/leads');
 
-leadRouter.post('/addLead',leadController.addLead )
-leadRouter.post('/getLeadsByOfInstitute',leadController.getLeadsByOfInstitute )
-leadRouter.post('/getSingleLead',leadController.getSingleLead )
-leadRouter.post('/updateLead',leadController.updateLead )
-leadRouter.post('/deleteLead',leadController.deleteLead )
+leadRouter.post('/addLead', leadController.addLead);
+leadRouter.post('/getLeadsByOfInstitute', leadController.getLeadsByOfInstitute);
+leadRouter.post('/getSingleLead', leadController.getSingleLead);
+leadRouter.post('/getSingleLeadToView', leadController.getSingleLeadToView);
+leadRouter.post('/updateLead', leadController.updateLead);
+leadRouter.post('/deleteLead', leadController.deleteLead);
 
-module.exports = leadRouter
+module.exports = leadRouter;
