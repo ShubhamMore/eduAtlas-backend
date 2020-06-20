afc3d5ea4ad8e12d1800863799786ac449bca3ab
Shubham More <shubhammore1796@gmail.com>
2020-06-20 14:07:03 +0530

    eduatlas

--- src/routes/notification.js
+++ src/routes/notification.js
@@ -4,5 +4,6 @@ const checkAuth = require('../middleware/checkAuth');
 
 mentorRouter.post('/deleteNotification', checkAuth, notificationController.deleteNotification);
 mentorRouter.post('/getNotifications', checkAuth, notificationController.getNotifications);
+mentorRouter.post('/seenNotification', checkAuth, notificationController.seenNotification);
 
 module.exports = mentorRouter;
