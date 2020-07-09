cccd298a9d45f5ffc4e8e3bb7d958414753365ee
Akash Kulkarni <akashkulkarni796@gmail.com>
2020-07-09 11:45:30 +0530

    akash

--- src/controller/studentDashboard.js
+++ src/controller/studentDashboard.js
@@ -83,6 +83,7 @@ exports.getStudentDashboard = async (req, res) => {
       {
         $project: {
           'instituteDetails.instituteId': 1,
+          instituteName: '$instituteCourse.basicInfo.name',
           batchCode: '$instituteCourse.batch.batchCode',
         },
       },
@@ -195,7 +196,7 @@ exports.getStudentDashboard = async (req, res) => {
             $eq: ['$batchId', '$schedule.batchId'],
           },
           'schedule.days.select': true,
-          'schedule.days.date': { $gte: '2020-07-10' },
+          'schedule.days.date': { $gte: currentDate },
         },
       },
       {
@@ -312,6 +313,242 @@ exports.getStudentDashboard = async (req, res) => {
     res.status(200).send({
       announcements,
       test,
+      schedule,
+    });
+  } catch (error) {
+    errorHandler(error, res);
+  }
+};
+
+exports.studentInstituteDashboard = async (req, res) => {
+  try {
+    const date = new Date();
+    const currentDate =
+      date.getFullYear() +
+      '-' +
+      appendZero(date.getMonth() + 1) +
+      '-' +
+      appendZero(date.getDate()) +
+      'T00:00:00';
+    console.log(currentDate);
+
+    const tests = await Student.aggregate([
+      {
+        $unwind: '$institutDetails',
+      },
+      {
+        $match: {
+          eduAtlasId: req.user.eduAtlasId,
+          'instituteDetails.instituteId': req.body.instituteId,
+          'instituteDetails.active': true,
+        },
+      },
+      {
+        $lookup: {
+          from: 'tests',
+          localField: 'instituteDetails.instituteId',
+          foreignField: 'instituteId',
+          as: 'test',
+        },
+      },
+      { $unwind: '$test' },
+      {
+        $match: {
+          $expr: {
+            $eq: ['$instituteDetails.batchId', '$test.batchId'],
+          },
+        },
+      },
+      {
+        $addFields: {
+          'instituteDetails.instituteId': {
+            $toObjectId: '$instituteDetails.instituteId',
+          },
+          batchId: {
+            $toObjectId: '$instituteDetails.batchId',
+          },
+        },
+      },
+      {
+        $lookup: {
+          from: 'institutes',
+          localField: 'instituteDetails.instituteId',
+          foreignField: '_id',
+          as: 'instituteCourse',
+        },
+      },
+      {
+        $unwind: '$instituteCourse',
+      },
+      { $unwind: '$instituteCourse.batch' },
+      {
+        $match: {
+          $expr: {
+            $eq: ['$instituteCourse.batch._id', '$batchId'],
+          },
+          'test.date': {
+            $gte: currentDate,
+          },
+        },
+      },
+      {
+        $project: {
+          test: 1,
+          batchCode: '$instituteCourse.batch.batchCode',
+        },
+      },
+    ]);
+
+    const schedule = await Student.aggregate([
+      {
+        $unwind: '$institutDetails',
+      },
+      {
+        $match: {
+          eduAtlasId: req.user.eduAtlasId,
+          'instituteDetails.instituteId': req.body.instituteId,
+          'instituteDetails.active': true,
+        },
+      },
+      {
+        $lookup: {
+          from: 'schedules',
+          localField: 'instituteDetails.instituteId',
+          foreignField: 'instituteId',
+          as: 'schedule',
+        },
+      },
+      { $unwind: '$schedule' },
+      { $unwind: '$schedule.days' },
+      {
+        $match: {
+          $expr: {
+            $eq: ['$instituteDetails.batchId', '$schedule.batchId'],
+          },
+          'schedule.days.date': {
+            $gte: currentDate,
+          },
+        },
+      },
+      {
+        $addFields: {
+          'instituteDetails.instituteId': {
+            $toObjectId: '$instituteDetails.instituteId',
+          },
+          batchId: {
+            $toObjectId: '$instituteDetails.batchId',
+          },
+        },
+      },
+      {
+        $lookup: {
+          from: 'institutes',
+          localField: 'instituteDetails.instituteId',
+          foreignField: '_id',
+          as: 'instituteCourse',
+        },
+      },
+      {
+        $unwind: '$instituteCourse',
+      },
+      { $unwind: '$instituteCourse.batch' },
+      {
+        $match: {
+          $expr: {
+            $eq: ['$instituteCourse.batch._id', '$batchId'],
+          },
+        },
+      },
+      {
+        $project: {
+          lecture: '$schedule.days',
+          batchCode: '$instituteCourse.batch.batchCode',
+        },
+      },
+    ]);
+
+    const announcements = await Student.aggregate([
+      {
+        $unwind: '$instituteDetails',
+      },
+      {
+        $match: {
+          eduAtlasId: req.user.eduAtlasId,
+          'instituteDetails.instituteId': req.body.instituteId,
+          'instituteDetails.active': true,
+        },
+      },
+      {
+        $addFields: {
+          'instituteDetails.instituteId': {
+            $toObjectId: '$instituteDetails.instituteId',
+          },
+        },
+      },
+      {
+        $lookup: {
+          from: 'institutes',
+          localField: 'instituteDetails.instituteId',
+          foreignField: '_id',
+          as: 'instituteCourse',
+        },
+      },
+      {
+        $unwind: '$instituteCourse',
+      },
+      {
+        $addFields: {
+          batchId: {
+            $toObjectId: '$instituteDetails.batchId',
+          },
+        },
+      },
+      {
+        $unwind: '$instituteCourse.batch',
+      },
+      {
+        $match: {
+          $expr: {
+            $and: [
+              {
+                $eq: ['$instituteCourse.batch._id', '$batchId'],
+              },
+            ],
+          },
+        },
+      },
+      {
+        $project: {
+          'instituteDetails.instituteId': 1,
+          batchCode: '$instituteCourse.batch.batchCode',
+        },
+      },
+      {
+        $lookup: {
+          from: 'announcements',
+          localField: 'instituteDetails.instituteId',
+          foreignField: 'instituteId',
+          as: 'announcements',
+        },
+      },
+      {
+        $unwind: '$announcements',
+      },
+      {
+        $unwind: '$announcements.batchCodes',
+      },
+      {
+        $match: {
+          $expr: {
+            $eq: ['$batchCode', '$announcements.batchCodes'],
+          },
+        },
+      },
+    ]);
+
+    res.status(200).send({
+      test,
+      announcements,
       schedule,
     });
   } catch (error) {
